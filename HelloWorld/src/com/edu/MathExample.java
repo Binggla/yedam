f34ed159f8a0b52a7a2afe7c8729f6ae185ba054
jee <jee@JEECOM>
2022-03-14 20:08:14 +0900

    5th commit

---
+++ HelloWorld/src/com/edu/MathExample.java
@@ -0,0 +1,32 @@
+package com.edu;
+
+import java.util.Scanner;
+
+public class MathExample {
+
+	public static void main(String[] args) {
+		
+		Scanner snc = new Scanner(System.in);
+		
+		int num, num2, hap, cha, gob, mok, na;
+		
+		System.out.println("첫 번째 정수를 입력하세요.");
+		num = snc.nextInt();
+		System.out.println("두 번째 정수를 입력하세요.");
+		num2 = snc.nextInt();
+		
+		hap = num + num2;
+		cha = num - num2;
+		gob = num * num2;
+		mok = num / num2;
+		na = num % num2;
+		
+		System.out.println("두 수의 합 : " + hap);
+		System.out.println("두 수의 차 : " + cha);
+		System.out.println("두 수의 곱 : " + gob);
+		System.out.println("두 번째 수로 첫 번째 수를 나눴을 때 몫 : " + mok);
+		System.out.println("두 번째 수로 첫 번째 수를 나눴을 때 나머지 : " + na);
+
+	}
+
+}
